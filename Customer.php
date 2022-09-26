2c92ebbe908fd42c58d4e31f43f08db0e98f7edf
Rayfy07 <90688771+Rayfy07@users.noreply.github.com>
2022-09-26 15:31:23 +0200

    BOURDON Thibaut class Customer
    Creation of customer class.

---
+++ Customer.php
@@ -0,0 +1,46 @@
+<?php
+
+    class Customer 
+    {
+        private int $id;
+        private string $code;
+        private string $name;
+        private string $notes;
+
+        public function getId(): int
+        {
+            return $this->id;
+        }
+        public function setId(string $id): void
+        {
+            $this->id = $id;
+        }
+
+        public function getCode(): string
+        {
+            return $this->code;
+        }
+        public function setCode(string $code): void
+        {
+            $this->code = $code;
+        }
+
+        public function getName(): string
+        {
+            return $this->name;
+        }
+        public function setName(string $name): void
+        {
+            $this->name = $name;
+        }
+
+        public function getNotes(): string
+        {
+            return $this->notes;
+        }
+        public function setNotes(string $notes): void
+        {
+            $this->notes = $notes;
+        }
+    }
+?>
