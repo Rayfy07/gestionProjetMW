278f5f351f367dfaf72cc38ab1314e2d55f9303e
Antony013 <83763776+Antony013@users.noreply.github.com>
2022-09-26 16:03:57 +0200

    Add files via upload

---
+++ Environment.php
@@ -0,0 +1,103 @@
+<?php
+
+    require_once __DIR__."/Project";
+
+    class environment
+    {
+        public function __construct(private int $id, private string $name, private string $link, private string $ipAdresse, private int $sshPort, private string $sshUserName, private string $phpMyAdminLink, private bool $ipRestriction, private Project $projectId)
+        {
+
+        }
+
+        public function getId(): int
+        {
+            return $this->id;
+        }
+
+        public function setId(int $newId): void
+        {
+            $this->id = $newId;
+        }
+
+        public function getName(): string
+        {
+            return $this->name;
+        }
+
+        public function setName(string $newName): void
+        {
+            $this->name = $newName;
+        }
+
+        public function getLink(): string
+        {
+            return $this->link;
+        }
+
+        public function setLink(string $newLink): void
+        {
+            $this->link = $newLink;
+        }
+
+        public function getIpAdresse(): string
+        {
+            return $this->ipAdresse;
+        }
+
+        public function setIpAdresse(string $newIpAdresse): void
+        {
+            $this->ipAdresse = $newIpAdresse;
+        }
+
+        public function getSshPort(): int
+        {
+            return $this->sshPort;
+        }
+
+        public function setSshPort(int $newSshPort): void
+        {
+            $this->sshPort = $newSshPort;
+        }
+
+        public function getSshUserName(): string
+        {
+            return $this->sshUserName;
+        }
+
+        public function setSshUserName(string $newSshUserName): void
+        {
+            $this->sshUserName = $newSshUserName;
+        }
+
+        public function getPhpMyAdminLink(): string
+        {
+            return $this->phpMyAdminLink;
+        }
+
+        public function setPhpMyAdminLink(string $newPhpMyAdminLink): void
+        {
+            $this->phpMyAdminLink = $newPhpMyAdminLink;
+        }
+
+        public function getIpRestriction(): bool
+        {
+            return $this->ipRestriction;
+        }
+
+        public function setIpRestriction(bool $newIpRestriction): void
+        {
+            $this->ipRestriction = $newIpRestriction;
+        }
+
+        public function getProjectId(): Project
+        {
+            return $this->projectId;
+        }
+
+        public function setProjectId(Project $newProjectId): void
+        {
+            $this->projectId = $newProjectId;
+        }
+    }
+
+?>
