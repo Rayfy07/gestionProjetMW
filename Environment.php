6101871efdb98bc20293d3051422a1409c492762
Rayfy07 <90688771+Rayfy07@users.noreply.github.com>
2022-10-03 17:05:41 +0200

    Add files via upload

--- Environment.php
+++ Environment.php
@@ -1,103 +1,80 @@
 <?php
 
-    require_once __DIR__."/Project.php";
+require_once __DIR__."/Project.php";
+require_once __DIR__."/Trait.php";
 
-    class environment
+class environment implements EstIdInterface
+{
+    use IdTrait;
+    use NameTrait;
+    public function __construct(int $id, string $name, private string $link, private string $ipAddress, private int $sshPort, private string $sshUserName, private string $phpMyAdminLink, private bool $ipRestriction, private Project $project)
     {
-        public function __construct(private int $id, private string $name, private string $link, private string $ipAdresse, private int $sshPort, private string $sshUserName, private string $phpMyAdminLink, private bool $ipRestriction, private Project $project)
-        {
-
-        }
-
-        public function getId(): int
-        {
-            return $this->id;
-        }
-
-        public function setId(int $newId): void
-        {
-            $this->id = $newId;
-        }
-
-        public function getName(): string
-        {
-            return $this->name;
-        }
-
-        public function setName(string $newName): void
-        {
-            $this->name = $newName;
-        }
-
-        public function getLink(): string
-        {
-            return $this->link;
-        }
-
-        public function setLink(string $newLink): void
-        {
-            $this->link = $newLink;
-        }
-
-        public function getIpAdresse(): string
-        {
-            return $this->ipAdresse;
-        }
-
-        public function setIpAdresse(string $newIpAdresse): void
-        {
-            $this->ipAdresse = $newIpAdresse;
-        }
-
-        public function getSshPort(): int
-        {
-            return $this->sshPort;
-        }
-
-        public function setSshPort(int $newSshPort): void
-        {
-            $this->sshPort = $newSshPort;
-        }
-
-        public function getSshUserName(): string
-        {
-            return $this->sshUserName;
-        }
-
-        public function setSshUserName(string $newSshUserName): void
-        {
-            $this->sshUserName = $newSshUserName;
-        }
-
-        public function getPhpMyAdminLink(): string
-        {
-            return $this->phpMyAdminLink;
-        }
-
-        public function setPhpMyAdminLink(string $newPhpMyAdminLink): void
-        {
-            $this->phpMyAdminLink = $newPhpMyAdminLink;
-        }
-
-        public function getIpRestriction(): bool
-        {
-            return $this->ipRestriction;
-        }
-
-        public function setIpRestriction(bool $newIpRestriction): void
-        {
-            $this->ipRestriction = $newIpRestriction;
-        }
-
-        public function getProject(): Project
-        {
-            return $this->project;
-        }
-
-        public function setProject(Project $newProject): void
-        {
-            $this->project = $newProject;
-        }
+        $this->id = $id;
+        $this->name = $name;
     }
+
+    public function getLink(): string
+    {
+        return $this->link;
+    }
+    public function setLink(string $newLink): void
+    {
+        $this->link = $newLink;
+    }
+
+    public function getIpAddress(): string
+    {
+        return $this->ipAddress;
+    }
+    public function setIpAddress(string $newIpAddress): void
+    {
+        $this->ipAddress = $newIpAddress;
+    }
+
+    public function getSshPort(): int
+    {
+        return $this->sshPort;
+    }
+    public function setSshPort(int $newSshPort): void
+    {
+        $this->sshPort = $newSshPort;
+    }
+
+    public function getSshUserName(): string
+    {
+        return $this->sshUserName;
+    }
+    public function setSshUserName(string $newSshUserName): void
+    {
+        $this->sshUserName = $newSshUserName;
+    }
+
+    public function getPhpMyAdminLink(): string
+    {
+        return $this->phpMyAdminLink;
+    }
+    public function setPhpMyAdminLink(string $newPhpMyAdminLink): void
+    {
+        $this->phpMyAdminLink = $newPhpMyAdminLink;
+    }
+
+    public function getIpRestriction(): bool
+    {
+        return $this->ipRestriction;
+    }
+    public function setIpRestriction(bool $newIpRestriction): void
+    {
+        $this->ipRestriction = $newIpRestriction;
+    }
+
+    public function getProject(): Project
+    {
+        return $this->project;
+    }
+    public function setProject(Project $newProject): void
+    {
+        $this->project = $newProject;
+    }
+}
 
 ?>
