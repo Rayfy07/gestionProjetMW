d8657b5259c67ed76f00be44b5e41ed4b29593b8
Rayfy07 <90688771+Rayfy07@users.noreply.github.com>
2022-10-03 17:04:33 +0200

    Add files via upload

--- Contact.php
+++ Contact.php
@@ -1,66 +1,45 @@
 <?php 
 
-    require_once __DIR__."/Customer.php";
-    require_once __DIR__."/Host.php";
+require_once __DIR__."/Customer.php";
+require_once __DIR__."/Host.php";
+require_once __DIR__."/Trait.php";
 
-    class Contact 
+class Contact implements EstIdInterface
+{
+    use IdTrait;
+    use HostCustomerTrait;
+
+    public function __construct(int $id, private string $email, private string $phoneNumber, private string $role, Host $host, Customer $customer)
     {
-        public function __construct(private int $id, private string $email, private string $phoneNumber, private string $role, private Host $host, private Customer $customer)
-        {
-            
-        }
-
-        public function getId(): int
-        {
-            return $this->id;
-        }
-        public function setId(string $newId): void
-        {
-            $this->id = $newId;
-        }
-
-        public function getEmail(): string
-        {
-            return $this->email;
-        }
-        public function setEmail(string $newEmail): void
-        {
-            $this->email = $newEmail;
-        }
-
-        public function getPhoneNumber(): string
-        {
-            return $this->phoneNumber;
-        }
-        public function setPhoneNumber(string $newPhoneNumber): void
-        {
-            $this->phoneNumber = $newPhoneNumber;
-        }
-
-        public function getRole(): string
-        {
-            return $this->role;
-        }
-        public function setRole(string $newRole): void
-        {
-            $this->role = $newRole;
-        }
-
-        public function getHost(): Host
-        {
-            return $this->host;
-        }
-        public function setHost(Host $newHost): void
-        {
-            $this->host = $newHost;
-        }
-
-        public function getCustomer(): Customer
-        {
-            return $this->customer;
-        }
-        public function setCustomer(Customer $newCustomer): void
-        {
-            $this->customer = $newCustomer;
-        }
+        $this->id = $id;
+        $this->host = $host;
+        $this->customer = $customer;
     }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+    public function setEmail(string $newEmail): void
+    {
+        $this->email = $newEmail;
+    }
+
+    public function getPhoneNumber(): string
+    {
+        return $this->phoneNumber;
+    }
+    public function setPhoneNumber(string $newPhoneNumber): void
+    {
+        $this->phoneNumber = $newPhoneNumber;
+    }
+
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+    public function setRole(string $newRole): void
+    {
+        $this->role = $newRole;
+    }
+}
