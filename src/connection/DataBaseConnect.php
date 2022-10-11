7b7213040aa26b89c621f512afdee73a52ed747f
Maxime Batista <90443919+BatMaxou@users.noreply.github.com>
2022-10-11 13:22:54 +0200

    Add files via upload

---
+++ src/connection/DataBaseConnect.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace App\connection;
+
+use PDO, PDOExeption;
+
+class DataBaseConnect{
+
+	private static $dbHost= "localhost";
+	private static $dbName = "test";
+	private static $dbUser = "root";
+	private static $dbUserPassword = "";
+
+	private static $connection = null;
+
+	public static function connect(){
+		try{
+			self::$connection = new PDO("mysql:host=".self::$dbHost.";dbname=".self::$dbName,self::$dbUser,self::$dbUserPassword);
+		}
+		catch(PDOExeption $e){
+			die($e->getMessage());
+		}
+		return self::$connection;
+	}
+
+	public static function disconnect(){
+		self::$connection = null;
+	}
+}
+
+?>
